a5ac313aca48a55fd4f5d92631800eac9809544f
andforce <86118@163.com>
2014-11-16 19:45:04 +0800

    add for android 5.0

--- src/pl/droidsonroids/gif/GifImageView.java
+++ src/pl/droidsonroids/gif/GifImageView.java
@@ -1,9 +1,7 @@
 package pl.droidsonroids.gif;
 
 import android.annotation.TargetApi;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.res.AssetFileDescriptor;
 import android.content.res.Resources;
 import android.content.res.Resources.NotFoundException;
 import android.net.Uri;
@@ -14,133 +12,127 @@ import android.widget.ImageView;
 import java.io.IOException;
 
 /**
- * An {@link ImageView} which tries treating background and src as
- * {@link GifDrawable}
- * 
+ * An {@link ImageView} which tries treating background and src as {@link GifDrawable}
+ *
  * @author koral--
  */
 public class GifImageView extends ImageView {
+    static final String ANDROID_NS = "http://schemas.android.com/apk/res/android";
 
-	static final String ANDROID_NS = "http://schemas.android.com/apk/res/android";
+    /**
+     * A corresponding superclass constructor wrapper.
+     *
+     * @param context
+     * @see ImageView#ImageView(Context)
+     */
+    public GifImageView(Context context) {
+        super(context);
+    }
 
-	/**
-	 * A corresponding superclass constructor wrapper.
-	 * 
-	 * @see ImageView#ImageView(Context)
-	 */
-	public GifImageView(Context context) {
-		super(context);
-	}
+    /**
+     * Like equivalent from superclass but also try to interpret src and background
+     * attributes as {@link GifDrawable}.
+     *
+     * @param context
+     * @param attrs
+     * @see ImageView#ImageView(Context, AttributeSet)
+     */
+    public GifImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        trySetGifDrawable(attrs, getResources());
+    }
 
-	/**
-	 * Like eqivalent from superclass but also try to interpret src and
-	 * background attributes as {@link GifDrawable}.
-	 * 
-	 * @see ImageView#ImageView(Context, AttributeSet)
-	 */
-	public GifImageView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		trySetGifDrawable(attrs, getResources());
-	}
+    /**
+     * Like equivalent from superclass but also try to interpret src and background
+     * attributes as GIFs.
+     *
+     * @param context
+     * @param attrs
+     * @param defStyle
+     * @see ImageView#ImageView(Context, AttributeSet, int)
+     */
+    public GifImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        trySetGifDrawable(attrs, getResources());
+    }
 
-	/**
-	 * Like eqivalent from superclass but also try to interpret src and
-	 * background attributes as GIFs.
-	 * 
-	 * @see ImageView#ImageView(Context, AttributeSet, int)
-	 */
-	public GifImageView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		trySetGifDrawable(attrs, getResources());
-	}
+    /**
+     * Like equivalent from superclass but also try to interpret src and background
+     * attributes as GIFs.
+     *
+     * @param context
+     * @param attrs
+     * @param defStyle
+     * @param defStyleRes
+     * @see ImageView#ImageView(Context, AttributeSet, int, int)
+     */
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public GifImageView(Context context, AttributeSet attrs, int defStyle, int defStyleRes) {
+        super(context, attrs, defStyle, defStyleRes);
+        trySetGifDrawable(attrs, getResources());
+    }
 
-	@Override
-	public void setImageResource(int resId) {
-		setResource(true, resId, getResources());
-	}
+    @Override
+    public void setImageResource(int resId) {
+        setResource(true, resId, getResources());
+    }
 
-	@Override
-	public void setBackgroundResource(int resId) {
-		setResource(false, resId, getResources());
-	}
+    @Override
+    public void setBackgroundResource(int resId) {
+        setResource(false, resId, getResources());
+    }
 
-	void trySetGifDrawable(AttributeSet attrs, Resources res) {
-		if (attrs != null && res != null && !isInEditMode()) {
-			int resId = attrs.getAttributeResourceValue(ANDROID_NS, "src", -1);
-			if (resId > 0 && "drawable".equals(res.getResourceTypeName(resId))) {
-				setResource(true, resId, res);
-			}
+    void trySetGifDrawable(AttributeSet attrs, Resources res) {
+        if (attrs != null && res != null && !isInEditMode()) {
+            int resId = attrs.getAttributeResourceValue(ANDROID_NS, "src", -1);
+            if (resId > 0 && "drawable".equals(res.getResourceTypeName(resId)))
+                setResource(true, resId, res);
 
-			resId = attrs.getAttributeResourceValue(ANDROID_NS, "background", -1);
-			if (resId > 0 && "drawable".equals(res.getResourceTypeName(resId))) {
-				setResource(false, resId, res);
-			}
-		}
-	}
+            resId = attrs.getAttributeResourceValue(ANDROID_NS, "background", -1);
+            if (resId > 0 && "drawable".equals(res.getResourceTypeName(resId)))
+                setResource(false, resId, res);
+        }
+    }
 
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-	@SuppressWarnings("deprecation")
-	// new method not avalilable on older API levels
-	void setResource(boolean isSrc, int resId, Resources res) {
-		try {
-			GifDrawable d = new GifDrawable(res, resId);
-			if (isSrc) {
-				setImageDrawable(d);
-			} else if (Build.VERSION.SDK_INT >= 16) {
-				setBackground(d);
-			} else {
-				setBackgroundDrawable(d);
-			}
-			return;
-		} catch (IOException ignored) {
-			// ignored
-		} catch (NotFoundException ignored) {
-			// ignored
-		}
-		if (isSrc) {
-			super.setImageResource(resId);
-		} else {
-			super.setBackgroundResource(resId);
-		}
-	}
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @SuppressWarnings("deprecation")
+        //new method not available on older API levels
+    void setResource(boolean isSrc, int resId, Resources res) {
+        try {
+            GifDrawable d = new GifDrawable(res, resId);
+            if (isSrc)
+                setImageDrawable(d);
+            else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                setBackground(d);
+            else
+                setBackgroundDrawable(d);
+            return;
+        } catch (IOException | NotFoundException ignored) {
+            //ignored
+        }
+        if (isSrc)
+            super.setImageResource(resId);
+        else
+            super.setBackgroundResource(resId);
+    }
 
-	/**
-	 * Sets the content of this GifImageView to the specified Uri. If uri
-	 * destination is not a GIF then
-	 * {@link android.widget.ImageView#setImageURI(android.net.Uri)} is called
-	 * as fallback. For supported URI schemes see:
-	 * {@link android.content.ContentResolver#openAssetFileDescriptor(android.net.Uri, String)}
-	 * . <code>file</code> scheme is always supported.
-	 * 
-	 * @param uri
-	 *            The Uri of an image
-	 */
-	@Override
-	public void setImageURI(Uri uri) {
-		GifDrawable gd = null;
-		if (uri != null) {
-			final String scheme = uri.getScheme();
-			try {
-				if (ContentResolver.SCHEME_FILE.equals(scheme)) {
-					gd = new GifDrawable(uri.getPath());
-				} else {
-					AssetFileDescriptor afd = getContext().getContentResolver().openAssetFileDescriptor(uri, "r");
-					if (afd != null) {
-						try {
-							gd = new GifDrawable(afd);
-						} catch (IOException ex) {
-							afd.close();
-						}
-					}
-				}
-			} catch (IOException ex) {
-				// ignored
-			}
-		}
-		if (gd != null) {
-			setImageDrawable(gd);
-		} else {
-			super.setImageURI(uri);
-		}
-	}
+    /**
+     * Sets the content of this GifImageView to the specified Uri.
+     * If uri destination is not a GIF then {@link android.widget.ImageView#setImageURI(android.net.Uri)}
+     * is called as fallback.
+     * For supported URI schemes see: {@link android.content.ContentResolver#openAssetFileDescriptor(android.net.Uri, String)}.
+     *
+     * @param uri The Uri of an image
+     */
+    @Override
+    public void setImageURI(Uri uri) {
+        if (uri != null)
+            try {
+                setImageDrawable(new GifDrawable(getContext().getContentResolver(), uri));
+                return;
+            } catch (IOException ignored) {
+                //ignored
+            }
+        super.setImageURI(uri);
+    }
 }
--- src/pl/droidsonroids/gif/GifTextView.java
+++ src/pl/droidsonroids/gif/GifTextView.java
@@ -16,98 +16,116 @@ import java.io.IOException;
  * {@code android:drawableStart} and {@code android:drawableEnd} from XML are
  * not supported but can be set using
  * {@link #setCompoundDrawablesRelativeWithIntrinsicBounds(int, int, int, int)}
- * 
+ *
  * @author koral--
  */
 public class GifTextView extends TextView {
 
-	/**
-	 * @param context
-	 */
-	public GifTextView(Context context) {
-		super(context);
-	}
+    /**
+     * A corresponding superclass constructor wrapper.
+     * @param context
+     */
+    public GifTextView(Context context) {
+        super(context);
+    }
 
-	/**
-	 * @param context
-	 * @param attrs
-	 */
-	public GifTextView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		parseAttrs(attrs);
-	}
+    /**
+     * Like equivalent from superclass but also try to interpret compound drawables defined in XML
+     * attributes as GIFs.
+     * @see TextView#TextView(Context, AttributeSet)
+     * @param context
+     * @param attrs
+     */
+    public GifTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        parseAttrs(attrs);
+    }
 
-	/**
-	 * @param context
-	 * @param attrs
-	 * @param defStyle
-	 */
-	public GifTextView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		parseAttrs(attrs);
-	}
+    /**
+     * Like equivalent from superclass but also try to interpret compound drawables defined in XML
+     * attributes as GIFs.
+     * @see TextView#TextView(Context, AttributeSet, int)
+     * @param context
+     * @param attrs
+     * @param defStyle
+     */
+    public GifTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        parseAttrs(attrs);
+    }
 
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-	private void parseAttrs(AttributeSet attrs) {
-		if (attrs != null) {
-			Drawable left = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableLeft", 0)), right = getGifOrDefaultDrawable(attrs
-					.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableRight", 0)), top = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(
-					GifImageView.ANDROID_NS, "drawableTop", 0)), bottom = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS,
-					"drawableBottom", 0)), start = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableStart", 0)), end = getGifOrDefaultDrawable(attrs
-					.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableEnd", 0));
-			setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+    /**
+     * Like equivalent from superclass but also try to interpret compound drawables defined in XML
+     * attributes as GIFs.
+     *
+     * @param context
+     * @param attrs
+     * @param defStyle
+     * @param defStyleRes
+     * @see TextView#TextView(Context, AttributeSet, int, int)
+     */
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public GifTextView(Context context, AttributeSet attrs, int defStyle, int defStyleRes) {
+        super(context, attrs, defStyle, defStyleRes);
+        parseAttrs(attrs);
+    }
 
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-				setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
-			}
-			setBackgroundInternal(getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "background", 0)));
-		}
-	}
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void parseAttrs(AttributeSet attrs) {
+        if (attrs != null) {
+            Drawable left = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableLeft", 0)), right = getGifOrDefaultDrawable(attrs
+                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableRight", 0)), top = getGifOrDefaultDrawable(attrs
+                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableTop", 0)), bottom = getGifOrDefaultDrawable(attrs
+                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableBottom", 0)), start = getGifOrDefaultDrawable(attrs
+                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableStart", 0)), end = getGifOrDefaultDrawable(attrs
+                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableEnd", 0));
+            setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
 
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-	// setBackground
-	@SuppressWarnings("deprecation")
-	// setBackgroundDrawable
-	private void setBackgroundInternal(Drawable bg) {
-		if (Build.VERSION.SDK_INT >= 16) {
-			setBackground(bg);
-		} else {
-			setBackgroundDrawable(bg);
-		}
-	}
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+                setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+            setBackgroundInternal(getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "background", 0)));
+        }
+    }
 
-	@Override
-	public void setBackgroundResource(int resid) {
-		setBackgroundInternal(getGifOrDefaultDrawable(resid));
-	}
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    // setBackground
+    @SuppressWarnings("deprecation")
+    // setBackgroundDrawable
+    private void setBackgroundInternal(Drawable bg) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            setBackground(bg);
+        else
+            setBackgroundDrawable(bg);
+    }
 
-	private Drawable getGifOrDefaultDrawable(int resId) {
-		if (resId == 0) {
-			return null;
-		}
-		final Resources resources = getResources();
-		if (!isInEditMode() && "drawable".equals(resources.getResourceTypeName(resId))) {
-			try {
-				return new GifDrawable(resources, resId);
-			} catch (IOException ignored) {
-				// ignored
-			} catch (NotFoundException ignored) {
-				// ignored
-			}
-		}
-		return resources.getDrawable(resId);
-	}
+    @Override
+    public void setBackgroundResource(int resid) {
+        setBackgroundInternal(getGifOrDefaultDrawable(resid));
+    }
 
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-	@Override
-	public void setCompoundDrawablesRelativeWithIntrinsicBounds(int start, int top, int end, int bottom) {
-		setCompoundDrawablesRelativeWithIntrinsicBounds(getGifOrDefaultDrawable(start), getGifOrDefaultDrawable(top), getGifOrDefaultDrawable(end),
-				getGifOrDefaultDrawable(bottom));
-	}
+    private Drawable getGifOrDefaultDrawable(int resId) {
+        if (resId == 0)
+            return null;
+        final Resources resources = getResources();
+        if (!isInEditMode() && "drawable".equals(resources.getResourceTypeName(resId)))
+            try {
+                return new GifDrawable(resources, resId);
+            } catch (IOException | NotFoundException ignored) {
+                // ignored
+            }
+        return resources.getDrawable(resId);
+    }
 
-	@Override
-	public void setCompoundDrawablesWithIntrinsicBounds(int left, int top, int right, int bottom) {
-		setCompoundDrawablesWithIntrinsicBounds(getGifOrDefaultDrawable(left), getGifOrDefaultDrawable(top), getGifOrDefaultDrawable(right),
-				getGifOrDefaultDrawable(bottom));
-	}
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void setCompoundDrawablesRelativeWithIntrinsicBounds(int start, int top, int end, int bottom) {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(getGifOrDefaultDrawable(start), getGifOrDefaultDrawable(top),
+                getGifOrDefaultDrawable(end), getGifOrDefaultDrawable(bottom));
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(int left, int top, int right, int bottom) {
+        setCompoundDrawablesWithIntrinsicBounds(getGifOrDefaultDrawable(left), getGifOrDefaultDrawable(top),
+                getGifOrDefaultDrawable(right), getGifOrDefaultDrawable(bottom));
+    }
 }
