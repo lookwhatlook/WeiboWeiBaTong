7d006f331010037c11b6ff8436c0cdecbd4804c8
wangdiyuan <wangdiyuan@smartisan.com>
2014-12-08 11:32:57 +0800

    refactor

--- src/org/zarroboogs/weibo/support/utils/ViewUtility.java
+++ src/org/zarroboogs/weibo/support/utils/ViewUtility.java
@@ -8,13 +8,13 @@ import android.view.View;
  */
 public class ViewUtility {
 
-	@SuppressWarnings({ "unchecked", "UnusedDeclaration" })
-	public static <T extends View> T findViewById(View view, int id) {
+	@SuppressWarnings("unchecked")
+    public static <T extends View> T findViewById(View view, int id) {
 		return (T) view.findViewById(id);
 	}
 
-	@SuppressWarnings({ "unchecked", "UnusedDeclaration" })
-	public static <T extends View> T findViewById(Activity activity, int id) {
+	@SuppressWarnings("unchecked")
+    public static <T extends View> T findViewById(Activity activity, int id) {
 		return (T) activity.findViewById(id);
 	}
 }
